7c3bfaab35ddc3e92ed19142eda576c5369b99b5
mohammed <mohammdneccar1999@gmail.com>
2025-07-30 16:40:25 +0300

    new version

---
+++ bin/stock_cli_app.dart
@@ -0,0 +1,5 @@
+import 'package:stock_cli_app/stock_cli_app.dart' as stock_cli_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${stock_cli_app.calculate()}!');
+}
---
+++ test/stock_cli_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:stock_cli_app/stock_cli_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
